5a0070f0e3206062980f54fefdc389e9189f12d2
Issaka <moussasidikouissaka019@gmail.com>
2024-07-05 18:12:15 +0000

    Initial commit

---
+++ src/test/java/com/esmt/issaka/IssakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmt.issaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
